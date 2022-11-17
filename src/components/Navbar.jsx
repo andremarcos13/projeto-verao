119ab0da0b411c9c62f9e43949be244937c367e7
Andre Cesar <andrmgc@gmail.com>
2022-11-17 11:29:43 -0300

    fix lint errors, add lint in package and changes navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
+
+function Navbar() {
+  return (
+    <Stack>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="bíceps e um coração"
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
+      </Link>
+    </Stack>
+  );
+}
+
+export default Navbar;
